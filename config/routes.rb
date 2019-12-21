41a8c71cf8873664c371f56bcb133b6148a8eec8
marlonfernando16 <marlon.fernando16@outlook.com>
2019-12-21 09:34:08 -0300

    version updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   get 'home/index'
   get '/', to: redirect('/home/index')
   root 'home#index'
